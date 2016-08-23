edc3f18e4c29a0f93479c20b3c9e07bcf990ce0a
combS8214808 <ksam34211sag+github@googlemail.com>
2016-08-23 09:46:30 +0900

    Vectorクラスと例外クラスを作成。
    和および内積、toString、hashCodeを実装。

---
+++ src/main/java/net/seiko_comb/combS8214808/joiss2016/NotSameVectorLengthException.java
@@ -0,0 +1,6 @@
+package net.seiko_comb.combS8214808.joiss2016;
+
+public class NotSameVectorLengthException extends RuntimeException {
+	private static final long serialVersionUID = 2789243658840365993L;
+	
+}
---
+++ src/main/java/net/seiko_comb/combS8214808/joiss2016/Vector.java
@@ -0,0 +1,74 @@
+package net.seiko_comb.combS8214808.joiss2016;
+
+import java.util.Arrays;
+
+public class Vector {
+	private double value[];
+	private int length;
+
+	private Vector(int length) {
+		this.value = new double[length];
+		this.length = length;
+	}
+
+	public static Vector zero(int length) {
+		return new Vector(length);
+	}
+	
+	public static Vector of(double... component){
+		Vector ret = new Vector(component.length);
+		System.arraycopy(component, 0, ret.value, 0, component.length);
+		return ret;
+	}
+
+	public Vector plus(Vector vector) {
+		checkVectorLength(this, vector);
+		Vector ret = new Vector(length);
+		for (int i = 0; i < length; i++)
+			ret.value[i] = this.value[i] + vector.value[i];
+		return ret;
+	}
+
+	public double product(Vector vector) {
+		checkVectorLength(this, vector);
+		double ret = 0;
+		for (int i = 0; i < length; i++)
+			ret += this.value[i] * vector.value[i];
+		return ret;
+	}
+
+	private void checkVectorLength(Vector v1, Vector v2) {
+		if (v1.length != v2.length)
+			throw new NotSameVectorLengthException();
+	}
+
+	@Override
+	public String toString() {
+		return Arrays.toString(value);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + length;
+		result = prime * result + Arrays.hashCode(value);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vector other = (Vector) obj;
+		if (length != other.length)
+			return false;
+		if (!Arrays.equals(value, other.value))
+			return false;
+		return true;
+	}
+}
